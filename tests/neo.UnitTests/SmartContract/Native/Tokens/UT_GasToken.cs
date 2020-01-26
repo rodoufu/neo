2aa226de6d8f094705faeba6c8550a5ec1b15944
Rodolfo Araujo <rodoufu@gmail.com>
2020-01-26 16:41:30 -0300

    Fix gas token unit test.

--- tests/neo.UnitTests/SmartContract/Native/Tokens/UT_GasToken.cs
+++ tests/neo.UnitTests/SmartContract/Native/Tokens/UT_GasToken.cs
@@ -38,107 +38,106 @@ namespace Neo.UnitTests.SmartContract.Native.Tokens
         [TestMethod]
         public void Check_BalanceOfTransferAndBurn()
         {
-            // TODO @rodoufu fix this
-//            var snapshot = Blockchain.Singleton.GetSnapshot();
-//            snapshot.PersistingBlock = new Block() { Index = 1000 };
-//
-//            byte[] from = Contract.CreateMultiSigRedeemScript(Blockchain.StandbyValidators.Length / 2 + 1,
-//                Blockchain.StandbyValidators).ToScriptHash().ToArray();
-//
-//            byte[] to = new byte[20];
-//
-//            var keyCount = snapshot.Storages.GetChangeSet().Count();
-//
-//            NativeContract.NEO.Initialize(new ApplicationEngine(TriggerType.Application, null, snapshot, 0));
-//            var supply = NativeContract.GAS.TotalSupply(snapshot);
-//            supply.Should().Be(3000000000000000);
-//
-//            // Check unclaim
-//
-//            var unclaim = UT_NeoToken.Check_UnclaimedGas(snapshot, from);
-//            unclaim.Value.Should().Be(new BigInteger(600000000000));
-//            unclaim.State.Should().BeTrue();
-//
-//            // Transfer
-//
-//            NativeContract.NEO.Transfer(snapshot, from, to, BigInteger.Zero, true).Should().BeTrue();
-//            NativeContract.NEO.BalanceOf(snapshot, from).Should().Be(100_000_000);
-//            NativeContract.NEO.BalanceOf(snapshot, to).Should().Be(0);
-//
-//            NativeContract.GAS.BalanceOf(snapshot, from).Should().Be(3000600000000000);
-//            NativeContract.GAS.BalanceOf(snapshot, to).Should().Be(0);
-//
-//            // Check unclaim
-//
-//            unclaim = UT_NeoToken.Check_UnclaimedGas(snapshot, from);
-//            unclaim.Value.Should().Be(new BigInteger(0));
-//            unclaim.State.Should().BeTrue();
-//
-//            supply = NativeContract.GAS.TotalSupply(snapshot);
-//            supply.Should().Be(3000600000000000);
-//
-//            snapshot.Storages.GetChangeSet().Count().Should().Be(keyCount + 3); // Gas
-//
-//            // Transfer
-//
-//            keyCount = snapshot.Storages.GetChangeSet().Count();
-//
-//            NativeContract.GAS.Transfer(snapshot, from, to, 3000600000000000, false).Should().BeFalse(); // Not signed
-//            NativeContract.GAS.Transfer(snapshot, from, to, 3000600000000001, true).Should().BeFalse(); // More than balance
-//            NativeContract.GAS.Transfer(snapshot, from, to, 3000600000000000, true).Should().BeTrue(); // All balance
-//
-//            // Balance of
-//
-//            NativeContract.GAS.BalanceOf(snapshot, to).Should().Be(3000600000000000);
-//            NativeContract.GAS.BalanceOf(snapshot, from).Should().Be(0);
-//
-//            snapshot.Storages.GetChangeSet().Count().Should().Be(keyCount + 1); // All
-//
-//            // Burn
-//
-//            var engine = new ApplicationEngine(TriggerType.Application, null, snapshot, 0);
-//            keyCount = snapshot.Storages.GetChangeSet().Count();
-//
-//            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
-//                NativeContract.GAS.Burn(engine, new UInt160(to), BigInteger.MinusOne));
-//
-//            // Burn more than expected
-//
-//            Assert.ThrowsException<InvalidOperationException>(() =>
-//                NativeContract.GAS.Burn(engine, new UInt160(to), new BigInteger(3000600000000001)));
-//
-//            // Real burn
-//
-//            NativeContract.GAS.Burn(engine, new UInt160(to), new BigInteger(1));
-//
-//            NativeContract.GAS.BalanceOf(snapshot, to).Should().Be(3000599999999999);
-//
-//            keyCount.Should().Be(snapshot.Storages.GetChangeSet().Count());
-//
-//            // Burn all
-//
-//            NativeContract.GAS.Burn(engine, new UInt160(to), new BigInteger(3000599999999999));
-//
-//            (keyCount - 1).Should().Be(snapshot.Storages.GetChangeSet().Count());
-//
-//            // Bad inputs
-//
-//            NativeContract.GAS.Transfer(snapshot, from, to, BigInteger.MinusOne, true).Should().BeFalse();
-//            NativeContract.GAS.Transfer(snapshot, new byte[19], to, BigInteger.One, false).Should().BeFalse();
-//            NativeContract.GAS.Transfer(snapshot, from, new byte[19], BigInteger.One, false).Should().BeFalse();
+            var snapshot = TestBlockchain.Container.Blockchain.GetSnapshot();
+            snapshot.PersistingBlock = new Block() { Index = 1000 };
+
+            byte[] from = Contract.CreateMultiSigRedeemScript(Blockchain.StandbyValidators.Length / 2 + 1,
+                Blockchain.StandbyValidators).ToScriptHash().ToArray();
+
+            byte[] to = new byte[20];
+
+            var keyCount = snapshot.Storages.GetChangeSet().Count();
+
+            NativeContract.NEO.Initialize(new ApplicationEngine(TriggerType.Application, null, snapshot, 0));
+            var supply = NativeContract.GAS.TotalSupply(snapshot);
+            supply.Should().Be(3000000000000000);
+
+            // Check unclaim
+
+            var unclaim = UT_NeoToken.Check_UnclaimedGas(snapshot, from);
+            unclaim.Value.Should().Be(new BigInteger(600000000000));
+            unclaim.State.Should().BeTrue();
+
+            // Transfer
+
+            NativeContract.NEO.Transfer(snapshot, from, to, BigInteger.Zero, true).Should().BeTrue();
+            NativeContract.NEO.BalanceOf(snapshot, from).Should().Be(100_000_000);
+            NativeContract.NEO.BalanceOf(snapshot, to).Should().Be(0);
+
+            NativeContract.GAS.BalanceOf(snapshot, from).Should().Be(3000600000000000);
+            NativeContract.GAS.BalanceOf(snapshot, to).Should().Be(0);
+
+            // Check unclaim
+
+            unclaim = UT_NeoToken.Check_UnclaimedGas(snapshot, from);
+            unclaim.Value.Should().Be(new BigInteger(0));
+            unclaim.State.Should().BeTrue();
+
+            supply = NativeContract.GAS.TotalSupply(snapshot);
+            supply.Should().Be(3000600000000000);
+
+            snapshot.Storages.GetChangeSet().Count().Should().Be(keyCount + 3); // Gas
+
+            // Transfer
+
+            keyCount = snapshot.Storages.GetChangeSet().Count();
+
+            NativeContract.GAS.Transfer(snapshot, from, to, 3000600000000000, false).Should().BeFalse(); // Not signed
+            NativeContract.GAS.Transfer(snapshot, from, to, 3000600000000001, true).Should().BeFalse(); // More than balance
+            NativeContract.GAS.Transfer(snapshot, from, to, 3000600000000000, true).Should().BeTrue(); // All balance
+
+            // Balance of
+
+            NativeContract.GAS.BalanceOf(snapshot, to).Should().Be(3000600000000000);
+            NativeContract.GAS.BalanceOf(snapshot, from).Should().Be(0);
+
+            snapshot.Storages.GetChangeSet().Count().Should().Be(keyCount + 1); // All
+
+            // Burn
+
+            var engine = new ApplicationEngine(TriggerType.Application, null, snapshot, 0);
+            keyCount = snapshot.Storages.GetChangeSet().Count();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+                NativeContract.GAS.Burn(engine, new UInt160(to), BigInteger.MinusOne));
+
+            // Burn more than expected
+
+            Assert.ThrowsException<InvalidOperationException>(() =>
+                NativeContract.GAS.Burn(engine, new UInt160(to), new BigInteger(3000600000000001)));
+
+            // Real burn
+
+            NativeContract.GAS.Burn(engine, new UInt160(to), new BigInteger(1));
+
+            NativeContract.GAS.BalanceOf(snapshot, to).Should().Be(3000599999999999);
+
+            keyCount.Should().Be(snapshot.Storages.GetChangeSet().Count());
+
+            // Burn all
+
+            NativeContract.GAS.Burn(engine, new UInt160(to), new BigInteger(3000599999999999));
+
+            (keyCount - 1).Should().Be(snapshot.Storages.GetChangeSet().Count());
+
+            // Bad inputs
+
+            NativeContract.GAS.Transfer(snapshot, from, to, BigInteger.MinusOne, true).Should().BeFalse();
+            NativeContract.GAS.Transfer(snapshot, new byte[19], to, BigInteger.One, false).Should().BeFalse();
+            NativeContract.GAS.Transfer(snapshot, from, new byte[19], BigInteger.One, false).Should().BeFalse();
         }
 
         [TestMethod]
         public void Check_BadScript()
         {
-            // TODO @rodoufu fix this
-//            var engine = new ApplicationEngine(TriggerType.Application, null, Blockchain.Singleton.GetSnapshot(), 0);
-//
-//            var script = new ScriptBuilder();
-//            script.Emit(OpCode.NOP);
-//            engine.LoadScript(script.ToArray());
-//
-//            NativeContract.GAS.Invoke(engine).Should().BeFalse();
+            var engine = new ApplicationEngine(
+                TriggerType.Application, null, TestBlockchain.Container.Blockchain.GetSnapshot(), 0);
+
+            var script = new ScriptBuilder();
+            script.Emit(OpCode.NOP);
+            engine.LoadScript(script.ToArray());
+
+            NativeContract.GAS.Invoke(engine).Should().BeFalse();
         }
 
         [TestMethod]
@@ -159,28 +158,27 @@ namespace Neo.UnitTests.SmartContract.Native.Tokens
         [TestMethod]
         public void TestGetSysFeeAmount2()
         {
-            // TODO @rodoufu fix this
-//            var snapshot = Blockchain.Singleton.GetSnapshot();
-//            NativeContract.GAS.GetSysFeeAmount(snapshot, 0).Should().Be(new BigInteger(0));
-//            NativeContract.GAS.GetSysFeeAmount(snapshot, 1).Should().Be(new BigInteger(0));
-//
-//            byte[] key = BitConverter.GetBytes(1);
-//            StorageKey storageKey = new StorageKey
-//            {
-//                ScriptHash = NativeContract.GAS.Hash,
-//                Key = new byte[sizeof(byte) + key.Length]
-//            };
-//            storageKey.Key[0] = 15;
-//            key.CopyTo(storageKey.Key.AsSpan(1));
-//
-//            BigInteger sys_fee = new BigInteger(10);
-//            snapshot.Storages.Add(storageKey, new StorageItem
-//            {
-//                Value = sys_fee.ToByteArrayStandard(),
-//                IsConstant = true
-//            });
-//
-//            NativeContract.GAS.GetSysFeeAmount(snapshot, 1).Should().Be(sys_fee);
+            var snapshot = TestBlockchain.Container.Blockchain.GetSnapshot();
+            NativeContract.GAS.GetSysFeeAmount(snapshot, 0).Should().Be(new BigInteger(0));
+            NativeContract.GAS.GetSysFeeAmount(snapshot, 1).Should().Be(new BigInteger(0));
+
+            byte[] key = BitConverter.GetBytes(1);
+            StorageKey storageKey = new StorageKey
+            {
+                ScriptHash = NativeContract.GAS.Hash,
+                Key = new byte[sizeof(byte) + key.Length]
+            };
+            storageKey.Key[0] = 15;
+            key.CopyTo(storageKey.Key.AsSpan(1));
+
+            BigInteger sys_fee = new BigInteger(10);
+            snapshot.Storages.Add(storageKey, new StorageItem
+            {
+                Value = sys_fee.ToByteArrayStandard(),
+                IsConstant = true
+            });
+
+            NativeContract.GAS.GetSysFeeAmount(snapshot, 1).Should().Be(sys_fee);
         }
     }
 }
